1b54145704df674fc2f1ac474133b148d91dd083
Mootez Saad <msaad@calvert.research.cs.dal.ca>
2023-07-14 19:34:14 -0300

    fix errors and change tokenizer options in dataset class

--- model/dataset.py
+++ model/dataset.py
@@ -11,25 +11,25 @@ class BugBERTDataset(Dataset):
     def __init__(self, dataframe, brs_dir, tokenizer, transform=None, target_transform=None):
         self.samples = dataframe
         self.brs_dir = brs_dir
-        self.tokenizer = tokenzier
+        self.tokenizer = tokenizer
     
     def prepare_input(self, text):
         inputs = self.tokenizer.encode_plus(
         text, 
         return_tensors='pt', 
         add_special_tokens=True, 
-        pad_to_max_length=True,
+        padding='max_length',
         truncation=True
     )
-
         return inputs
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        anchor, positive, negative = list(map(lambda x: os.path.join(self.brs_dir, x), df.loc[idx, self.samples.columns].values))
+        file_names = self.samples.loc[idx, self.samples.columns].values[1:]
+        anchor, positive, negative = list(map(lambda x: os.path.join(self.brs_dir, x), file_names))
         raw_a, raw_p, raw_n = read_multiple_files(anchor, positive, negative)
-        item = tuple(prepare_input(x) for x in (raw_a, raw_p, raw_n))
+        item = tuple(self.prepare_input(x) for x in (raw_a, raw_p, raw_n))
 
         return item
