42c416527b8eba66592622c0c750e6b1ce75d4c9
Mootez Saad <msaad@calvert.research.cs.dal.ca>
2023-07-14 19:34:31 -0300

    refactor pooler

--- model/pooler.py
+++ model/pooler.py
@@ -9,9 +9,6 @@ class MeanPooling(nn.Module):
         super(MeanPooling, self).__init__()
         
     def forward(self, last_hidden_state, attention_mask):
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
-        sum_embeddings = torch.sum(last_hidden_state * input_mask_expanded, 1)
-        sum_mask = input_mask_expanded.sum(1)
-        sum_mask = torch.clamp(sum_mask, min=1e-9)
-        mean_embeddings = sum_embeddings / sum_mask
-        return mean_embeddings
+        attention_mask = attention_mask.squeeze(dim=1).unsqueeze(dim=2)
+        masked_hidden_states = (last_hidden_state * attention_mask).mean(dim=1)
+        return masked_hidden_states
