1680a6a15f5735bcb95bc59ca714fd0a20287ff9
Mootez Saad <msaad@calvert.research.cs.dal.ca>
2023-07-14 19:36:28 -0300

    fix lightning module forward functions

--- model/pl_bugbert.py
+++ model/pl_bugbert.py
@@ -7,25 +7,31 @@ from transformers import get_cosine_schedule_with_warmup
 
 class BugBERTModel(pl.LightningModule):
     def __init__(self, model, optimizer, num_train_steps):
-        super(SiameseModel, self).__init__()
+        super(BugBERTModel, self).__init__()
         self.model = model
         self.criterion = nn.TripletMarginLoss(margin=1.0, p=2)
         self.num_train_steps = num_train_steps
         self.optimizer = optimizer
 
-    def forward(self, x_a, x_p, x_n):
-        return self.model(x_a, x_p, x_n)
+    def forward(self, x):
+        return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        x_a, x_p, x_n = batch
-        embd_x_a, embd_x_p, embd_x_n = self.model(x_a, x_p, x_n)
+        embd_x_a, embd_x_p, embd_x_n = self.model(batch)
 
         loss = self.criterion(embd_x_a, embd_x_p, embd_x_n)
         self.log("train_loss", loss)
         return loss
 
+    def validation_step(self, batch, batch_idx):
+        embd_x_a, embd_x_p, embd_x_n = self.model(batch)
+
+        loss = self.criterion(embd_x_a, embd_x_p, embd_x_n)
+        self.log("val_loss", loss)
+        return loss
+
     def configure_optimizers(self):
-        scheduler = get_cosine_schedule_with_warmup(
-                self.optimizer, num_warmup_steps=0, num_training_steps=num_train_steps, num_cycles=cfg.num_cycles
+        lr_scheduler = get_cosine_schedule_with_warmup(
+                self.optimizer, num_warmup_steps=0, num_training_steps=self.num_train_steps, num_cycles=0.5
             ) 
-        return self.optimizer
+        return [self.optimizer], [lr_scheduler]
