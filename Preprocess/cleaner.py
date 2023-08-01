d42727a643f6127805689f4df52cb68220b8c056
Indranil Palit <ip1102@narval4.narval.calcul.quebec>
2023-07-31 21:41:16 -0400

    feat: added preprocessing

---
+++ Preprocess/cleaner.py
@@ -0,0 +1,75 @@
+import json, pandas as pd, pickle as pkl
+from tqdm import tqdm
+
+
+class Preprocess:
+
+    def __init__(self, path) -> None:
+        self.path = path
+
+    
+    def basic_eda():
+        pass
+
+    @staticmethod
+    def __jsonl_gen(jsonl_data):
+        for line in jsonl_data:
+            yield json.loads(line)
+
+    @classmethod
+    def __get_desc_br(cls,bug_ids, jira_path, mongo_path):
+        jira_data = pd.read_csv(jira_path, delimiter=";")
+        mongo_data = open(mongo_path).readlines()
+        mongo_ids = []
+
+        for line in cls.__jsonl_gen(mongo_data):
+            mongo_ids.append(line["issue_id"])
+        
+        assert len(mongo_data) == len(mongo_ids)
+
+        final_bug_id_set =  set()
+        
+        for _id in tqdm(bug_ids):
+            cont = True
+            if _id in mongo_ids:
+                if json.loads(mongo_data[mongo_ids.index(_id)])['description']:
+                    final_bug_id_set.add(_id)
+                cont=False
+
+            if cont:
+                idx = jira_data.index[jira_data['issue_id']==_id]
+                if len(jira_data.loc[idx]) != 0:
+                    if len(str(jira_data.loc[idx]['description'])) != 0:
+                        final_bug_id_set.add(_id)
+
+
+        return final_bug_id_set
+
+    @classmethod
+    def preprocess_json(cls, path=None, jira_path=None, mongo_path=None):
+
+        with open(cls(path).path) as f:
+            data_json = json.load(f)
+        
+        bug_ids = set()
+        
+        for _id in data_json.keys():
+            bug_ids.add(_id)
+
+            for __id in data_json[_id]:
+                bug_ids.add(__id)
+
+        print(f"Number of unique Bug IDs: {len(bug_ids)}")
+
+        bug_id_desc_set = Preprocess.__get_desc_br(bug_ids, jira_path, mongo_path)
+
+        print(f"Length of BR with description: {len(bug_id_desc_set)}")
+
+
+        pkl.dump(bug_id_desc_set,open("bug_ids_desc.pkl","+wb"))
+
+        return bug_ids
+
+Preprocess.preprocess_json("/home/ip1102/projects/def-tusharma/ip1102/bugbert_preprocess/dbrs_no_hl.json",
+                           "/home/ip1102/projects/def-tusharma/ip1102/bugbert_preprocess/issues_jira.csv",
+                           "/home/ip1102/projects/def-tusharma/ip1102/bugbert_preprocess/issues_mongodb.jsonl")
---
+++ Preprocess/generate.py
@@ -0,0 +1,101 @@
+import json
+import pandas as pd
+from tqdm import tqdm
+import random, pickle as pkl
+
+SET_SIZE = 500000
+
+def read_json_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"Error: File not found at '{file_path}'")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON format in file '{file_path}'")
+        return None
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+        return None
+    
+    
+def generate_all_triplets(duplicates_dict):
+    triplets = []
+    anchors = list(duplicates_dict.keys())
+    k = 1000000
+
+    for anchor in tqdm(anchors):
+        positives = duplicates_dict[anchor]
+        negatives = [key for key in anchors if key != anchor and key not in positives]
+
+        for positive in positives:
+            for negative in negatives:
+                if len(triplets) == k:
+                    return triplets
+                triplets.append((anchor, positive, negative))
+
+    return triplets
+
+def split_dict(dictionary):
+    
+    keys = list(dictionary.keys())
+    random.shuffle(keys)
+    
+    
+    total_items = len(keys)
+    train_size = int(total_items * 0.75)
+    valid_size = int(total_items * 0.2)
+    test_size = total_items - train_size - valid_size
+    
+    
+    train_dict = {}
+    valid_dict = {}
+    test_dict = {}
+    
+    
+    for i, key in enumerate(keys):
+        if i < train_size:
+            train_dict[key] = dictionary[key]
+        elif i < train_size + valid_size:
+            valid_dict[key] = dictionary[key]
+        else:
+            test_dict[key] = dictionary[key]
+    
+    return train_dict, valid_dict, test_dict
+
+
+file_path = "./dbrs_no_hl.json"
+duplicates_dict = read_json_file(file_path)
+
+br_with_desc = pkl.load(open("./bug_ids_desc.pkl", "+rb"))
+
+duplicates_dict_with_desc = dict.fromkeys(br_with_desc,None)
+
+for key in tqdm(duplicates_dict_with_desc.keys()):
+    if key in duplicates_dict.keys():
+        vals = [val for val in duplicates_dict[key]]
+        if len(vals) != 0:
+            duplicates_dict_with_desc[key] = vals
+
+# json.dump({k: v for k, v in duplicates_dict_with_desc.items() if v}, open("./dbrs_no_hl_with_desc.json","+w"))
+
+duplicate_json = json.load(open("./dbrs_no_hl_with_desc.json","+r"))
+
+train_data, valid_data, test_data = split_dict(duplicate_json)
+
+
+triplets_dataset = generate_all_triplets(train_data)
+
+print(len(triplets_dataset))
+
+random.shuffle(triplets_dataset)
+
+sampled = random.sample(triplets_dataset, SET_SIZE)
+
+df = pd.DataFrame(sampled)
+
+df.columns = ["anchor", "positive", "negative"]
+
+df.to_csv("dbrs_dataset_with_desc.csv", index=None)
